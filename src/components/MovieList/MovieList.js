e241d1af059b6dda72769edad6b5b006636e7d78
Admin <Badr.Ouaddah.07@gmail.com>
2023-07-17 11:35:50 +0100

    Implement hover effect for images containers using CSS

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./MovieList.css";
 
+
 const MovieList = (props) => {
   return (
     <>
       {props.movies.map((movie, index) => (
         <div className="col-sm-3" key={index} >
-          <div className=" d-flex justify-content-start m-3">
+          <div className="image-container d-flex justify-content-start m-3">
             <img src={movie.Poster} alt="Movie" />
+            <div className="overlay d-flex align-items-centerjustify-content"></div>
           </div>
         </div>
       ))}
