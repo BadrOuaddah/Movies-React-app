fac8e501b516c73c8bfa7697eb999c4d7cf96bcb
Admin <Badr.Ouaddah.07@gmail.com>
2023-07-19 13:41:03 +0100

    Add RemoveFavourites component

---
+++ src/components/RemoveFavourites/RemoveFavourites.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './RemoveFavourites.css';
+
+export default function RemoveFavourites() {
+  return (
+    <div>
+      RemoveFavourites component !
+    </div>
+  )
+}
